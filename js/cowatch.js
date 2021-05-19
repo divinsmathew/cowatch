d6f5149810908c9bb5dd00e7af62efe6bf36ce3f
Divins Mathew <divinsmathew@gmail.com>
2021-05-19 22:07:32 +0530

    browser notifications

--- js/cowatch.js
+++ js/cowatch.js
@@ -73,7 +73,7 @@ async function onload()
                 case "denied":
                     browserNotificationCheckbox.style.display = "none"
                     let label = document.querySelector("#notificationSettings label")
-                    label.innerHTML = "<b>Please enable the permission manually.</b>"
+                    label.innerHTML = "<b>Please grand the permission manually.</b>"
                     setTimeout(() =>
                     {
                         label.innerHTML = "Receive browser notifications"
